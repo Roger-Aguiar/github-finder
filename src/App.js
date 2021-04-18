43a25afc4090b06c04561ffe5a4836193c5fb128
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-04-18 08:19:08 -0300

    It creates the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
-class App extends Component 
-{
+class App extends React.Component 
+{ 
   render()
-  {
+  {        
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className="App">        
+        <Navbar></Navbar>       
       </div>
     );
   }  
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+export class Navbar extends Component 
+{
+    static defaultProps =
+    {
+        title: "Github Finder",
+        icon: "fab fa-github-square"
+    }
+
+    static propTypes =
+    {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    }
+
+    render() 
+    {
+        return (
+            <nav className="navbar bg-primary">
+                <h1>                    
+                    <i className = {this.props.icon}></i> {this.props.title}                    
+                </h1>                
+            </nav>
+        )
+    }
+}
+
+export default Navbar
