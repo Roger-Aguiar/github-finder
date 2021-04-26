f297797b5077ca885f26b6169819e06666c9c80c
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-04-26 17:09:52 -0300

    It changes the homepage route

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component
 
             <Switch>
               <Route 
-                exact path = '/' 
+                exact path = '/github-finder' 
                 render = {props => (
                   <Fragment>
 
